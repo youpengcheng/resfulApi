d271d64e05e23c4dc6b416d9419d2d49277159e3
ypc <2412038772@qq.com>
2019-11-30 15:15:02 +0800

    second

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ import path from 'path'
 export const System = {
   API_server_type: 'http://', // API服务器协议类型,包含"http://"或"https://"
   API_server_host: 'localhost', // API服务器暴露的域名地址,请勿添加"http://"
-  API_server_port: '3000', // API服务器监听的端口号
+  API_server_port: '3111', // API服务器监听的端口号
   HTTP_server_type: 'http://', // HTTP服务器协议类型,包含"http://"或"https://"
   HTTP_server_host: 'www.XXX.com', // HTTP服务器地址,请勿添加"http://" （即前端调用使用的服务器地址，如果是APP请设置为 * ）
   HTTP_server_port: '65534', // HTTP服务器端口号
